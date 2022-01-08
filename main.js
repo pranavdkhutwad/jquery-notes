edb48c1ec2ccd4fd0f2ef735fceab96d17342c9b
Ganesh Khutwad <pranavdkhutwad@gmail.com>
2022-01-08 11:09:17 +0530

    adding jquery files

---
+++ main.js
@@ -0,0 +1,12 @@
+$( document ).ready(function(){
+    // jQuery Effects
+    // 1. Element show, hide, toggle
+
+    // 2. fadein fadeout fadetoggle
+
+    // 3. slideUp slideDown slideToggle
+
+   $('#data').click(function() {
+       $('.content').slideToggle(1000);
+   });
+});
